dba752d652305c05961bf0c921d91661c51718d3
Amritlms1 <163141553+Amritlms1@users.noreply.github.com>
2024-07-17 15:43:38 +0530

    Add files via upload

---
+++ Core java/Inheritance/src/inheritance/Child.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Child extends Inheritance{
+public int height;
+
+public Child(int gear, int speed, int height) {
+	super(gear, speed);
+	this.height = height;
+}
+}
---
+++ Core java/Inheritance/src/inheritance/Inheritance.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Inheritance {
+	public int gear;
+	public int speed;
+	public Inheritance(int startgear, int startspeed) {
+		gear = startgear;
+	    speed = startspeed;
+	}
+	
+}
